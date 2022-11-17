25ef6d26641d31122934f81103b049baa9ce4ad6
-TOXIC- <93470009+TxcToxic@users.noreply.github.com>
2022-11-17 19:24:00 +0100

    Fixed now it works

--- RPC.py
+++ RPC.py
@@ -6,5 +6,7 @@ clientID = 123
 rpc = pypresence.Presence(clientID)
 rpc.connect()
 
+rpc.update(details="Top Text", state="Low Text", large_image="NAME", large_text="Large Image Text", start=time, buttons=[{"label": "Website", "url": "https://cft-devs.xyz"}, {"label": "Clicker", "url": "https://cft-devs.xyz/clicker"}])
+
 while True:
-    rpc.update(details="Top Text", state="Low Text", large_image="NAME", large_text="Large Image Text", start=time)
+    time.sleep(30) # Can only update rich presence every 15 seconds
