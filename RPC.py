6df37b7a81fb1968e1ccf5f21773b6c0d027ea9c
-TOXIC- <93470009+TxcToxic@users.noreply.github.com>
2022-11-17 19:24:25 +0100

    Fix

--- RPC.py
+++ RPC.py
@@ -7,6 +7,7 @@ rpc = pypresence.Presence(clientID)
 rpc.connect()
 
 rpc.update(details="Top Text", state="Low Text", large_image="NAME", large_text="Large Image Text", start=time, buttons=[{"label": "Website", "url": "https://cft-devs.xyz"}, {"label": "Clicker", "url": "https://cft-devs.xyz/clicker"}])
+# You can only make 2 buttons!
 
 while True:
     time.sleep(30) # Can only update rich presence every 15 seconds
