a165fc41c43467ac9867b4084dde331f21349495
TxcToxic <93470009+TxcToxic@users.noreply.github.com>
2022-11-25 11:48:06 +0100

    now it shall works better

--- spammer.py
+++ spammer.py
@@ -25,7 +25,8 @@ else:
 ### CONFIG ###
 
 channelID = "ID"  # Channel ID
-account = {"authorization": "TOKEN"}
+account = {"authorization": "TOKEN",
+           "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"}
 message = "You bastards"
 howmany = 100
 
