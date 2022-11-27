c7317f17c9af9fabc1d4fe924696b4458161c4e9
TxcToxic <93470009+TxcToxic@users.noreply.github.com>
2022-11-27 22:26:45 +0100

    cleanup

--- joiner.py
+++ joiner.py
@@ -5,6 +5,6 @@ inviteID = "bxsN3avF"  # without https://discord.gg/
 url = f"https://discord.com/api/v9/invites/{inviteID}"
 account = {"authorization": "TOKEN",
            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"}
-r = requests.post(url=url, headers=account, json={})
+r = requests.post(url=url, headers=account)
 
 print(f"[!] Status Code: {r.status_code}")
